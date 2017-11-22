b0b38592992baa01ac2a4c265d49a7242f55828c
Charbel Kisso <charbelkisso@gmail.com>
2017-11-22 18:56:03 +0100

    Comments added

--- scipy_test.py
+++ scipy_test.py
@@ -1,12 +1,39 @@
+
+
+"""
+Software Testing Project - Scipy TestUnit
+
+Participant:
+
+- Charbel Kisso
+- add you names
+
+"""
+
+"""
+importing important libraries for testing
+"""
 import unittest
 import numpy as np
+"""
+built-in assert function works with arrays and ndArrays 
+"""
 from numpy.testing import assert_equal
 
+"""
+functions to test:
+
+ToDo: add function here to build test cases for it  
+"""
 from scipy import misc, linalg, imag, signal
 
 
 class Scipy_Test(unittest.TestCase):
 
+
+    """
+    Scope to define global variable are used in the test unit
+    """
     def setUp(self):
 
         return
@@ -17,7 +44,6 @@ class Scipy_Test(unittest.TestCase):
         x = np.array([1.0, 2.0, 3.0])
         h = np.array([0.0, 1.0, 0.0, 0.0, 0.0])
 
-        print(x)
         assert_equal(signal.convolve(x, h),
                           [0.0, 1.0, 2.0, 3.0, 0.0, 0.0, 0.0])
 
