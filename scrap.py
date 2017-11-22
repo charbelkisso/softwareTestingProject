f965ee2b74b7c64638847656fa4e5025c10b239e
Charbel Kisso <charbelkisso@gmail.com>
2017-11-22 18:03:41 +0100

    some work

--- scrap.py
+++ scrap.py
@@ -1,20 +1,22 @@
 from numpy import *
+import numpy as np
 from scipy import signal, misc
 import matplotlib.pyplot as plt
+import unittest
 
+class test_scipy(unittest.TestCase):
 
-image = misc.face(gray=True).astype(float32)
-derfilt = array([1.0,-2,1.0],float32)
-ck = signal.cspline2d(image,8.0)
-deriv = signal.sepfir2d(ck, derfilt, [1]) +  signal.sepfir2d(ck, [1], derfilt)
+    def test_convolve(self):
 
+        x = np.array([1.0, 2.0, 3.0])
+        h = np.array([0.0, 1.0, 0.0, 0.0, 0.0])
 
-plt.figure()
-plt.imshow(image)
+        print(x)
+        self.assertEquals(signal.convolve(x, h),
+                          [ 0.0, 1.0, 2.0, 3.0, 0.0, 0.0, 0.0])
 
-plt.gray()
-plt.title("Original Image")
-plt.show()
+if __name__ == '__main__':
 
+    unittest.main()
 
 
