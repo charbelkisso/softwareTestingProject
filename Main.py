b1b1a7027528ddfb234c49bd0f8bb890fdb3f141
Charbel Kisso <charbelkisso@gmail.com>
2017-12-07 19:07:38 +0100

    adding new stucture for files

---
+++ Main.py
@@ -0,0 +1,16 @@
+from ndimage.gaussian_filter_test import *
+class Main():
+
+    def __init__(self):
+        self.gaussian = Gaussian_Filter_Test()
+
+    def main(self):
+        self.gaussian.main()
+
+
+if __name__ == '__main__':
+
+    mainEX = Main()
+    mainEX.main()
+
+    
--- ndimage/gaussian_filter_test.py
+++ ndimage/gaussian_filter_test.py
@@ -13,36 +13,44 @@ class Gaussian_Filter_Test(unittest.TestCase):
 
     Function: name gaussian_filter()
 
+    Guassian filter used for reduce noises into images. the input is presented as nd array
+    with n dimensions and the output is a nd array correspond to the filtered input with same 
+    dimensions.
+
     Test Plan:
     =========
+    1. testing the return result of the function. 
+        a. on one dimension input shall return one dimension output.
+        b. on two dimention input shall return two dimension output.
+        c. on 3X3X3 dimension input array
+    2. testing exeptions whene the function is called without parameters.
+    3. testing truncate option by counting the non-zero items in the result array.
 
-    1. testing the return result of the function 
-        a. on one dimension
-        b. on two dimention
-        the result test will 
-
-
-
-
-
+    since the output value of this function is an approximation of the gaussian secon deviration
+    to calculated by hand and compaire result is quit difficult and it doesn't give any testing benifits
     """
-    def setUp(self):
-        self.x = [11, 4, 5, 12, 6, 8]
-        self.y = np.arange(50, step=2)
-        self.res1 = []
-        
 
-    def test_filter_result_with_sigma(self):
-        self.res_test_1 = gaussian_filter(self.x, 1)
-        self.res_test_3 = gaussian_filter(self.x, 3)
-        self.res_test_8 = gaussian_filter(self.x, 8)
+    def setUp(self):
+        self.x = [11, 4, 5, 12, 6, 8, 25]
+        self.y = np.arange(50, step=2).reshape(5, 5)
+        self.z = (5 * np.random.random_sample(27)).reshape(3, 3, 3) 
         
-    def test_2d(self):
+        
+    """ Test Plan 1
+    """
+    def test_gaussian_output_shape(self):
+        self.assertEqual(gaussian_filter(self.x, 1).shape, (7,))
+        self.assertEqual(gaussian_filter(self.y, 1).shape, (5, 5))
+        self.assertEqual(gaussian_filter(self.z, 1).shape, (3, 3, 3))
+
+    """ Test Plan 2
+    """
+    def test_gaussian_no_input(self):
         return
 
-    def main():
+    def test_gaussian_truncate(self):
+
+    def main(self):
         unittest.main()
 
-if __name__ == '__main__':
-    Gaussian_Filter_Test.main()
     
