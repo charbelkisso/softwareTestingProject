a855317caf0a0ef918a29ed981406cdf6c35efed
Charbel Kisso <charbelkisso@gmail.com>
2018-01-08 14:21:03 +0100

    refactor

--- linalg_test/subspace_angles.py
+++ linalg_test/subspace_angles.py
@@ -23,15 +23,12 @@ class Test_subspace_angles(unittest.TestCase):
 	def testdefault(self):
 		#angle between orthogonal columns
 		np.testing.assert_almost_equal(np.rad2deg(linalg.subspace_angles(self.A[:,:2], self.A[:,2:])), self.P)
-		#Subspace angle of a matrix to itself should be zero (array([True, True])), but a matrix with orthogonal columns size 4 fail the test
 		np.testing.assert_array_equal(linalg.subspace_angles(self.A[:,:2], self.A[:,:2]) <= 2*np.finfo(float).eps, self.Z)
 		np.testing.assert_array_equal(linalg.subspace_angles(self.A2[:,:2], self.A2[:,:2]) <= 2*np.finfo(float).eps, self.Z)
 		#angle between non-orthogonal columns
 		np.testing.assert_almost_equal(np.rad2deg(linalg.subspace_angles(self.X[:, :2], self.X[:, [2]])), np.array([55.83245]))
 
 	def testinvalid(self):
-		#self.assertRaisesRegexp(ValueError, "expected 2D array, got shape ()", linalg.subspace_angles, -1, 0)
-		#self.assertRaisesRegexp(ValueError, "On entry to DGESDD parameter number 5 had an illegal value", linalg.subspace_angles, self.zero, self.zero)
 		assert_raises(ValueError, linalg.subspace_angles, -1, 0)
 		assert_raises(ValueError, linalg.subspace_angles, self.zero, self.zero)
 
