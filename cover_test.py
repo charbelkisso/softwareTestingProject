a18042506a000355bed5df7177b5a8ffb7a1d777
charbelkisso <charbelkisso@gmail.com>
2018-01-30 17:30:55 +0100

    White box

--- cover_test.py
+++ cover_test.py
@@ -24,28 +24,64 @@ class Test_Least_Squares(unittest.TestCase):
 		self.res_test = [0.19280596, 0.19130423, 0.12306063, 0.13607247]
 		self.res_unbound_self_test = ([  1.0117800995e+06,  -1.4075881180e+01,
 							-4.5790318134e+07, -2.8564061567e+07])
+		self.res_test_lm = ([ 1.0117800995e+06, -1.4075881180e+01, -4.5790318134e+07,
+        					-2.8564061567e+07])
+		self.res_temp = [9.40418096e-04,1.71030814e+02,-5.20017963e+00,8.09010276e+00]
+		self.res_temp_x = [0.2329967442,-0.5000017786,-0.0358009854,-0.2321008023]
 		self.res_unbound_test = ([  1.9070429319e+05,  -1.4075872912e+01,
 							-8.6307328042e+06, -5.3838624284e+06])
+
+		self.res_sparse = [ 2.5 , 3.8472489507,  5.1140583493,  5.4414322215]
 		self.u = np.array([4.0, 2.0, 1.0, 5.0e-1, 2.5e-1, 1.67e-1, 1.25e-1, 1.0e-1,
             				8.33e-2, 7.14e-2, 6.25e-2])
+
+		self.u_s = np.array([4.0, 2.0, 1.0, 6.25e-2])
+
 		self.y = np.array([1.957e-1, 1.947e-1, 1.735e-1, 1.6e-1, 8.44e-2, 6.27e-2,
             				4.56e-2, 3.42e-2, 3.23e-2, 2.35e-2, 2.46e-2])
+		self.y_s = np.array([1.957e-1, 1.947e-1,2.35e-2, 2.46e-2])
+
 		self.x0 = np.array([2.5, 3.9, 4.15, 3.9])
+		self.x0_s = np.array([2.5, 3.9])
+
+		self.x0_large = np.array([2,5,3,9,4,15,3,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5])
 		self.x0_complex = np.array([1+2j, 3+4j, 5+6j])
 		self.x0_ndim = np.array([
 			[1, 2, 3],
 			[4, 5, 6]
 		])
 
-	"""	
-	def cauchy(self,z, rho, cost_only):
+
+
+	def cauchy_x(self, z, rho= np.empty((3, 11)), cost_only=False):
+
 		rho[0] = np.log1p(z)
 		if cost_only:
 			return
 		t = 1 + z
 		rho[1] = 1 / t
 		rho[2] = -1 / t ** 2
-	"""
+		return rho
+
+	def cauchy_y(self, z, rho, cost_only=False):
+		rho[0] = np.log1p(z)
+		if cost_only:
+			return
+		t = 0* 1 + z
+		rho[1] = 0* 1 / t
+		rho[2] = 0* -1 / t ** 2
+
+	def cauchy_z(self, z, rho= np.empty((3, 11)), cost_only=False):
+
+		rho[0] = np.log1p(z)
+		if cost_only:
+			return z
+		t = 1 + z
+		rho[1] = 1 / t
+		rho[2] = -1 / t ** 2
+		return z
+
+
 	def model (self, x, u):
 
 		return x[0] * (u ** 2 + x[1] * u) / (u ** 2 + x[2] * u + x[3])
@@ -53,6 +89,14 @@ class Test_Least_Squares(unittest.TestCase):
 	def fun(self, x, u, y):
 
 		return self.model(x, u) - y
+
+	def fun_not_returning(self, x, u, y):
+		pass
+
+	def fun_ndim(self, x, u, y):
+
+		return self.x0_ndim
+
 
 	def jac(self, x, u, y):
 		J = np.empty((u.size, x.size))
@@ -62,6 +106,44 @@ class Test_Least_Squares(unittest.TestCase):
 		J[:, 1] = x[0] * u / den
 		J[:, 2] = -x[0] * num * u / den ** 2
 		J[:, 3] = -x[0] * num / den ** 2
+		return J
+
+	def jac_s(self, x, u, y):
+		J = np.empty((u.size, x.size))
+		den = u ** 2 + x[2] * u + x[3]
+		num = u ** 2 + x[1] * u
+		J[:, 0] = 10* num / den
+		J[:, 1] = 10* x[0] * u / den
+		J[:, 2] = 10* -x[0] * num * u / den ** 2
+		J[:, 3] = 10* -x[0] * num / den ** 2
+		return J
+
+	def jac_wrong(self, x, u, y):
+		J = np.empty((u.size, x.size-1))
+		den = u ** 2 + x[2] * u + x[3]
+		num = u ** 2 + x[1] * u
+		J[:, 0] = num / den
+		J[:, 1] = x[0] * u / den
+		J[:, 2] = -x[0] * num * u / den ** 2
+		#J[:, 3] = -x[0] * num / den ** 2
+		return J
+
+	def jac_zero(self, x, u, y):
+		J = np.empty((u.size, x.size))
+		den = u ** 2 + x[2] * u + x[3]
+		num = u ** 2 + x[1] * u
+		J[:, 0] = 0 * num / den
+		J[:, 1] = 0 * u / den
+		J[:, 2] = 0 * num * u / den ** 2
+		J[:, 3] = 0 * num / den ** 2
+		return J
+
+	def jac_sparse(self, x, u, y):
+		J = np.empty((u.size, x.size))
+		J = np.empty((u.size, x.size))
+		den = u ** 2 + x[2] * u + x[3]
+		num = u ** 2 + x[1] * u
+		J[:, 0] = 0 * num / den
 		return J
 
 	def jac_dunm(self, x, u, y):
@@ -99,12 +181,45 @@ class Test_Least_Squares(unittest.TestCase):
 		# x0 not in bound
 		assert_raises(ValueError, least_squares, self.fun, self.x0, bounds=(0, 2))
 		# method = lm and jac sparsity = none
-		assert_raises(ValueError, least_squares, self.fun, self.x0, method = 'lm', jac_sparsity = None, bounds=(0, 2))
+		assert_raises(ValueError, least_squares, self.fun, self.x0, method = 'lm', jac_sparsity = 'sparse matrix', bounds=(0, 2))
 		# method = lm and jac sparsity != none and tr_solver = exact
-		assert_raises(ValueError, least_squares, self.fun, self.x0, method = 'lm', jac_sparsity = 'sparse matrix' , tr_solver = 'exact', bounds=(0, 2))
+		#assert_raises(ValueError, least_squares, self.fun, self.x0, method = 'lm', jac_sparsity = 'sparse matrix' , tr_solver = 'exact', bounds=(0, 2))
 		# loss function is none
 		assert_raises(ValueError, least_squares, self.fun, self.x0, jac='3-point', bounds=(0, 100), loss=None, tr_solver=None,
 						args=(self.u, self.y))
+		# wrong shape of jacobian
+		assert_raises(ValueError, least_squares, self.fun, self.x0, jac=self.jac_wrong,bounds=(0, 100), method='dogbox',loss='linear',tr_solver='lsmr', args=(self.u, self.y),jac_sparsity='sparse matrix')
+
+		assert_raises(ValueError, least_squares, self.fun, self.x0, jac=self.jac_zero,bounds=(0, 100), method='lm',loss='linear', args=(self.u, self.y),jac_sparsity='sparse matrix')
+
+		assert_raises(ValueError, least_squares, self.fun, self.x0, jac=self.jac,bounds=(0, 100),x_scale=-1.0, method='dogbox',loss='linear',tr_solver='lsmr', args=(self.u, self.y),jac_sparsity='sparse matrix')
+
+		assert_raises(ValueError, least_squares, self.fun, self.x0,bounds=(0, 100), method='dogbox',loss='linear',tr_solver='lsmr', args=(self.u, self.y),jac_sparsity=self.x0)
+
+		assert_raises(TypeError, least_squares, self.fun, self.x0, jac='3-point',bounds=(0, 100), loss=self.cauchy_y, tr_solver=None, args=(self.u, self.y))
+
+		assert_raises(TypeError, least_squares, self.fun_not_returning, self.x0, jac=self.jac_zero,bounds=(0, 100), method='dogbox',loss='huber',tr_solver='lsmr', args=(self.u, self.y),jac_sparsity='sparse matrix')
+
+		#fun must rerun atmost-1d arrays
+		assert_raises(ValueError, least_squares, self.fun_ndim, self.x0,jac='cs', bounds=(0, 100),loss='linear',method='trf', args=(self.u, self.y),jac_sparsity=None, verbose=2)
+
+		#method='lm' does not support `jac_sparsity`.
+		assert_raises(ValueError, least_squares, self.fun, self.x0,diff_step=1e-8, method='lm',tr_solver='exact', jac_sparsity='sparse matrix', args=(self.u, self.y))
+
+		#tr_solver='exact' is incompatible with `jac_sparsity` when method !=lm
+		assert_raises(ValueError, least_squares, self.fun, self.x0,bounds=(0, 100), method='dogbox',tr_solver='exact', args=(self.u, self.y),jac_sparsity='sparse matrix')
+		#warn("The keyword 'regularize' in `tr_options` is not relevant "
+		assert_raises(AttributeError, least_squares, self.fun, self.x0,bounds=(0, 100), method='dogbox',tr_solver='lsmr', tr_options='regularize', args=(self.u, self.y))
+		#Method 'lm' doesn't work when the number of residuals is less than the number of variables
+		assert_raises(ValueError, least_squares, self.fun, self.x0_large, method='lm', args=(self.u, self.y))
+		#The return value of `loss` callable has wrong shape" both two cases
+		assert_raises(ValueError, least_squares, self.fun, self.x0,method='lm,', jac='3-point',bounds=(0, 100), loss=self.cauchy_z, tr_solver=None,
+							args=(self.u, self.y))
+		assert_raises(ValueError, least_squares, self.fun, self.x0, jac='3-point', bounds=(0, 100), loss=self.cauchy_z, tr_solver=None,
+							args=(self.u, self.y))
+
+
+
 	def test2_green(self):
 		"""
 		2: Green Test Path: This test will execute least_square function with default values
@@ -124,6 +239,14 @@ class Test_Least_Squares(unittest.TestCase):
 		res = least_squares(self.fun, self.x0,jac='cs', bounds=(0, 100),loss='linear',method='trf', args=(self.u, self.y),jac_sparsity=None, verbose=2)
 		assert_almost_equal(res.x, self.res_test,decimal=6)
 
+		res = least_squares(self.fun, self.x0,x_scale='jac', args=(self.u, self.y))
+		assert_almost_equal(res.x, self.res_temp_x,decimal=3)
+
+		res = least_squares(self.fun, self.x0,x_scale='jac', args=(self.u, self.y))
+		assert_almost_equal(res.x, self.res_temp_x,decimal=3)
+
+
+
 	def test4_yellow(self):
 		"""
 		4: Yellow Test Path: This test will cover:
@@ -131,6 +254,18 @@ class Test_Least_Squares(unittest.TestCase):
 		"""
 		res = least_squares(self.fun, self.x0, jac='2-point',method='lm',tr_solver='exact', args=(self.u, self.y))
 		assert_almost_equal(res.x, self.res_unbound_test,decimal=4)
+
+		res = least_squares(self.fun, self.x0, jac='2-point',diff_step=1e-8, method='lm',tr_solver='exact', args=(self.u, self.y))
+		assert_almost_equal(res.x, self.res_unbound_test,decimal=4)
+
+		res = least_squares(self.fun, self.x0, jac=self.jac,diff_step=1e-14,max_nfev=None, method='lm',tr_solver='exact', args=(self.u, self.y))
+		assert_almost_equal(res.x, self.res_test_lm,decimal=2)
+
+		#warn("jac='{0}' works equivalently to '2-point'
+		res = least_squares(self.fun, self.x0, jac='3-point', method='lm', args=(self.u, self.y))
+		assert_almost_equal(res.x, self.res_unbound_test,decimal=4)
+
+
 
 
 	def test5_cyan(self):
@@ -141,6 +276,11 @@ class Test_Least_Squares(unittest.TestCase):
 		res = least_squares(self.fun, self.x0, jac=self.jac,bounds=(0, 100), method='dogbox',loss='linear',tr_solver='lsmr', args=(self.u, self.y),jac_sparsity='sparse matrix')
 		assert_almost_equal(res.x, self.res_test,decimal=3)
 
+		res = least_squares(self.fun, self.x0, jac=self.jac_zero,bounds=(0, 100), method='dogbox',loss='huber',tr_solver='lsmr', args=(self.u, self.y),jac_sparsity='sparse matrix')
+		assert_almost_equal(res.x, self.x0,decimal=3)
+
+		res = least_squares(self.fun, self.x0, jac=self.jac_s,diff_step=20,args=(self.u_s, self.y_s))
+		assert_almost_equal(res.x, self.res_temp,decimal=3)
 
 	def test6_grey(self):
 		"""
@@ -159,13 +299,13 @@ class Test_Least_Squares(unittest.TestCase):
 							args=(self.u, self.y))
 		assert_almost_equal(res.x, self.res_test, decimal=3)
 
-		res = least_squares(self.fun, self.x0, jac='3-point', bounds=(0, 100), loss='arctan', tr_solver=None,
+		res = least_squares(self.fun, self.x0, jac='3-point',bounds=(0, 100), loss='arctan', tr_solver=None,
 							args=(self.u, self.y))
 		assert_almost_equal(res.x, self.res_test, decimal=3)
 
-#		res = least_squares(self.fun, self.x0, jac='3-point', bounds=(0, 100), loss=None, tr_solver=None,
-#						args=(self.u, self.y))
-#		assert_almost_equal(res.x, self.res_test, decimal=3)
+		res = least_squares(self.fun, self.x0, jac='3-point',bounds=(0, 100), loss=self.cauchy_x, tr_solver=None,
+							args=(self.u, self.y))
+		assert_almost_equal(res.x, self.res_test, decimal=5)
 
 	def runTest(self):
 		pass
@@ -175,4 +315,3 @@ class Test_Least_Squares(unittest.TestCase):
 
 if __name__ == '__main__':
 	unittest.main()
-
